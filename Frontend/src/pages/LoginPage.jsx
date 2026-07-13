2194d971290411111fe1ea1b56b611e8bb8af3d9
Rishikasiddhant <rishikasiddhant@gmail.com>
2026-07-13 13:51:55 +0530

    Update login page

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -24,6 +24,7 @@ const LoginPage = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    console.log("Button clicked!");
     const errs = validate()
     if (Object.keys(errs).length) { setErrors(errs); return }
     setIsLoading(true)
